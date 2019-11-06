882c222f02bd081131c531ab2211f10fa392e023
natsuya yamashiro <azuki728@me.com>
2019-11-06 10:57:19 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by NATSUYA YAMASHIRO on 2019/11/06.
+//  Copyright © 2019 natsuya.yamashiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
